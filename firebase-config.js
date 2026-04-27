d233fe3f4f6ba56715f6396e1911b17392b57ce4
Artmotor <34838889+Artmotor@users.noreply.github.com>
2026-04-27 20:00:20 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // firebase-config.js - ФИНАЛЬНАЯ версия
 const firebaseConfig = {
-  apiKey: "AIzaSyC9Gp359H5gVRE6DpDUp1md9o2uu1c5A7k",
-  authDomain: "vgau-lk.firebaseapp.com",
-  projectId: "vgau-lk",
-  storageBucket: "vgau-lk.firebasestorage.app",
-  messagingSenderId: "832671757947",
-  appId: "1:832671757947:web:07a01204dffefc30b2d4de",
-  measurementId: "G-7JKFLK0SSS"
+    apiKey: "AIzaSyC9Gp359H5gVRE6DpDUp1md9o2uu1c5A7k",
+    authDomain: "vgau-lk.firebaseapp.com",
+    projectId: "vgau-lk",
+    storageBucket: "vgau-lk.firebasestorage.app",
+    messagingSenderId: "832671757947",
+    appId: "1:832671757947:web:07a01204dffefc30b2d4de",
+    measurementId: "G-7JKFLK0SSS"
 };
 
 // Инициализация Firebase
